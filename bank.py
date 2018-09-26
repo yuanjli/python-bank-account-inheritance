c0b6c60de541810e2d8a211c42dc56ca015b1587
yuanjli <liyuanjienetwork@gmail.com>
2018-09-25 23:56:16 -0700

    added the bank_account class

--- bank.py
+++ bank.py
@@ -1,5 +1,15 @@
 class BankAccount:
-  pass
+  # pass
+  def __init__(self):
+  	self.balance = 0
+
+  def withdraw(self, amount):
+  	self.balance -= amount
+  	return self.balance
+
+  def deposit(self, amount):
+  	self.balance += amount
+  	return self.balance
 
 class ChildrensAccount:
   pass
